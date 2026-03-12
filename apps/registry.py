9fb018720383f8b8c29d44924b64c4abe85de660
neybarraz <neybarraz@gmail.com>
2026-03-12 18:26:16 -0300

    Update registry.py

--- apps/registry.py
+++ apps/registry.py
@@ -1,11 +1,16 @@
 # apps/registry.py
 APPS = {
-    # "app_01": {
-    #     "label": "App 01",
-    #     "module": "apps.app_01.app",
-    # },
+    "app_01": {
+         "label": "App 01",
+         "module": "apps.app_01.app",
+    },
     "app_02": {
         "label": "App 02",
         "module": "apps.app_02.app",
     },
+    "app_05": {
+        "label": "App 05",
+        "module": "apps.app_05.app",
+    },    
+
 }
